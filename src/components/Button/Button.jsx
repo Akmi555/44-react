8662c58d506285827120a3efcb4fd5fa09f0e987
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2024-07-04 22:49:41 +0200

    add lesson02

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+function Button() {
+  const nameButton = "Get";
+
+  return <button className="button-component">{nameButton} button</button>;
+}
+
+export default Button;
