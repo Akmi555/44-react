5d45fceda32dffed1a1f5b5d55079b6b8dee2fd0
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2024-07-05 23:39:47 +0200

    add lesson03

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import "./styles.css";
 
-function Button() {
-  const nameButton = "Get";
+function Button({ buttonName = "Get" }) {
+  // const nameButton = "Get";
 
-  return <button className="button-component">{nameButton} button</button>;
+  // return <button className="button-component">{nameButton} button</button>;
+  return <button className="button-component">{buttonName}</button>;
 }
 
 export default Button;
