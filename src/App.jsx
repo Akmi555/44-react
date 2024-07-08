efd3a47f54ad362a7431f2d38678d020ebda20d9
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2024-07-08 21:17:52 +0200

    add lesson04

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import "./App.css";
 
 //Lessons imports
 // import Lesson02 from "./lessons/Lessons02/Lesson02";
-import Lesson03 from "./lessons/Lesson03/Lesson03";
+// import Lesson03 from "./lessons/Lesson03/Lesson03";
+import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 //Homeworks imports
 // import Homework02 from "./homeworks/Homework02/Homework02";
+// import Homework03 from "./homeworks/Homework03/Homework03";
 
 function App() {
   return (
     <div className="App">
       {/* <Lesson02 /> */}
       {/* <Homework02/> */}
-      <Lesson03 />
+      {/* <Lesson03 /> */}
+      {/* <Homework03 /> */}
+      <Lesson04 />
     </div>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,14 @@
 import "./styles.css";
 
-function Button({ buttonName = "Get" }) {
+function Button({ name, type = "button", onButtonClick }) {
   // const nameButton = "Get";
 
   // return <button className="button-component">{nameButton} button</button>;
-  return <button className="button-component">{buttonName}</button>;
+  return (
+    <button className="button-component" type={type} onClick={onButtonClick}>
+      {name}
+    </button>
+  );
 }
 
 export default Button;
