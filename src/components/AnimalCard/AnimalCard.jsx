c975ad23c3ab4f216832a8c61140a914a6356067
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2024-07-09 22:20:57 +0200

    add lesson05

--- src/components/AnimalCard/AnimalCard.jsx
+++ src/components/AnimalCard/AnimalCard.jsx
@@ -1,22 +1,14 @@
 import "./styles.css";
 
-function AnimalCard({ animalName, species, role, animalImg, children }) {
+function AnimalCard({ animalData }) {
   return (
     <div className="animal-card-wrapper">
-      <h2>{animalName}</h2>
-      <p>{species}</p>
-      <p>{role}</p>
-      <img src={animalImg} />
-      {children}
+      <h2>{animalData.name}</h2>
+      <p>{animalData.species}</p>
+      <p>{animalData.role}</p>
+      <img src={animalData.image} />
     </div>
   );
 }
 
 export default AnimalCard;
-
-// const props = {
-//   animalName:
-//   species:
-//   role:
-//   animalImg:
-// }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 function Button({ name, type = "button", onButtonClick }) {
+  console.log('button component render - name prop changed')
   // const nameButton = "Get";
 
   // return <button className="button-component">{nameButton} button</button>;
